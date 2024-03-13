cc72703cefd7ec1640cba9abc01fcfbbc51f14e5
RahmanHanifs <128392694+RahmanHanifs@users.noreply.github.com>
2024-03-13 16:47:54 +0700

    Add files via upload

---
+++ Praktikum 4/src/Tugas Modul/org/Poligon/Poligon.java
@@ -0,0 +1,13 @@
+/*
+    Nama    : Rahman Hanif Aji Saputra
+    NIM     : 24060122140128
+    File    : Poligin.java
+*/
+package org.Poligon;
+
+public class Poligon {
+    protected int jumlahSisi;
+    public int getjumlahSisi(){
+        return this.jumlahSisi;
+    }
+}
